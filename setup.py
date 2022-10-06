fb842d731842c7f46fc3740df516a9d81551a299
Crateus <33150548+Crateus@users.noreply.github.com>
2022-10-06 15:18:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,9 +12,12 @@ setup(
     #url="http://enjoy-digital.fr",
     download_url="https://github.com/Crateus/litex_board_nexys_A7",
     #test_suite="test",
-    license="BSD",
+    #license="BSD",
     python_requires="~=3.6",
+    packages=find_packages(),
+    package_data={
+    	'litex_boards': ['litex_boards/**'],
+    },
     include_package_data=True,
-    packages=find_packages(where="litex-boards"),
     #package_dir={"": "litex-boards"},
 )
